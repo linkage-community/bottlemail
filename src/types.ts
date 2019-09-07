dcd329d19fef7ce158932da776664271a0c54dd7
otofune <otofune@otofune.me>
2019-09-07 17:23:01 +0900

    :up: Stop to expose Node type

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,28 @@
-export type Node<T> = {
+type Node<T> = {
   kind: T,
   value: string,
   raw: string,
 }
+
 export const TextKind = 'Text'
 // raw, value are same
 export type Text = Node<typeof TextKind>
+
 export const EmojiNameKind = 'EmojiName'
 // eg: raw => ':smile:', value => 'smile'
 export type EmojiName = Node<typeof EmojiNameKind>
+
 export const LinkKind = 'Link'
 // raw, value are same
 export type Link = Node<typeof LinkKind>
+
 export const MentionKind = 'Mention'
 // eg: raw => '@dolphin', value => 'dolphin'
 export type Mention = Node<typeof MentionKind>
+
 export type NodeList = Link | Text | Mention | EmojiName
 
+// helpers
 export const isText = (n: NodeList): n is Text => {
   return n.kind === TextKind
 }
